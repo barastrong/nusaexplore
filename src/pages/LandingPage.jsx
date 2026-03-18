85eb3d021b569c62f95bf684f836d40203d665d1
barastrong <145325312+barastrong@users.noreply.github.com>
2026-03-18 20:49:10 +0700

    first commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to Our Application</h1>
+      <p>This is the landing page of our application. Please explore the features and enjoy your experience!</p>
+    </div>
+  );
+}
+
+export default LandingPage;
